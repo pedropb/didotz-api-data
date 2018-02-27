431d22557e3872a7481fa0e3027751c4897b6b6c
Pedro Baracho <pedropb@trt3.jus.br>
2018-02-27 15:32:07 -0300

    Spring boot starter files

---
+++ src/main/java/br/com/nucleoti/DidotzApiDataApplication.java
@@ -0,0 +1,12 @@
+package br.com.nucleoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidotzApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidotzApiDataApplication.class, args);
+	}
+}
